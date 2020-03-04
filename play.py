b7674b2021e86f420b88a3f83b77eb511daa1edc
Dronch <dr0shik90@gmail.com>
2020-03-04 17:21:39 +0300

    beauty win congrats ;)

--- play.py
+++ play.py
@@ -3,8 +3,12 @@ from console_render import ConsoleRender
 
 
 g = Game(6, 7, ('A', 'B'), 4)
-g.play(
-    input_func=ConsoleRender.player_input,
-    win_callback=ConsoleRender.win,
-    error_callback=ConsoleRender.error
-)
+
+try:
+    g.play(
+        input_func=ConsoleRender.player_input,
+        win_callback=ConsoleRender.win,
+        error_callback=ConsoleRender.error
+    )
+except KeyboardInterrupt:
+    pass
